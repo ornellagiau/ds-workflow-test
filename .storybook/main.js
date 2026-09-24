cc0aa5b72c7540648af5420bb5828821eee5926b
Ornella Giau <ornellagiau@gmail.com>
2026-09-24 17:01:27 +0200

    Create main.js

---
+++ .storybook/main.js
@@ -0,0 +1,5 @@
+export default {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx)"],
+  addons: ["@storybook/addon-docs"],
+  framework: { name: "@storybook/react-vite", options: {} },
+};
