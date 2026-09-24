1028d757447e2ff410cea2148785dd6492396c18
Ornella Giau <ornellagiau@gmail.com>
2026-09-24 18:12:04 +0200

    Update main.js

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,5 +1,13 @@
+import remarkGfm from "remark-gfm";
+
 export default {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx)"],
-  addons: ["@storybook/addon-docs"],
+  addons: [
+    {
+      name: "@storybook/addon-docs",
+      // remark-gfm: Markdown tables in MDX render as real tables
+      options: { mdxPluginOptions: { mdxCompileOptions: { remarkPlugins: [remarkGfm] } } },
+    },
+  ],
   framework: { name: "@storybook/react-vite", options: {} },
 };
